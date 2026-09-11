0c99e4d04939ebfacda6329c6fc65a573c20e229
karunya-krishna <kg8113@srmist.edu.in>
2026-09-11 07:16:21 +0530

    Add customer name reversal solution

---
+++ ReverseCustomerName.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseCustomerName {
+    static String reverseCustomerName(String customerName) {
+        char[] chars = customerName.toCharArray();
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed.append(chars[i]);
+        }
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter customer name: ");
+        String customerName = scanner.nextLine();
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reverseCustomerName(customerName));
+        scanner.close();
+    }
+}
